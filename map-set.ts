ced547a790a38181ab33d2d0145842b768fa6528
Diprodip Das <diprodip755@gmail.com>
2026-07-21 15:41:04 +0600

    added map-set.ts

---
+++ map-set.ts
@@ -0,0 +1,20 @@
+//set
+
+// const data = new Set <string>()  // define the type=sr=string
+
+// data.add ("Dipro")
+// data.add(15)
+// data.add (true)
+
+// console.log(data)
+
+
+const players = new Map <string,number>()
+
+players.set("Naymer",10)
+players.set("Messi",10)
+players.set("Ronaldo", 7)
+
+console.log(players)
+
+
