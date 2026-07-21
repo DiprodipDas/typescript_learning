843d1384fe7d2e10af4b2dcdd5bf679eaeb09158
Diprodip Das <diprodip755@gmail.com>
2026-07-21 16:14:06 +0600

    added a example of map in  map-set.ts(juice order quantity checking)

--- map-set.ts
+++ map-set.ts
@@ -8,13 +8,30 @@
 
 // console.log(data)
 
+//map
+// const players = new Map <string,number>()
 
-const players = new Map <string,number>()
+// players.set("Naymer",10)
+// players.set("Messi",10)
+// players.set("Ronaldo", 7)
 
-players.set("Naymer",10)
-players.set("Messi",10)
-players.set("Ronaldo", 7)
+// console.log(players)
 
-console.log(players)
+
+const orders= new Map <string, number> ()
+
+function addOrder(juice:string){
+    const quantity= (orders.get(juice)?? 0)+1
+    orders.set(juice,quantity)
+}
+
+addOrder("lemon mint")
+addOrder("Mango")
+addOrder("lemon mint")
+addOrder("apple")
+addOrder("apple")
+
+
+console.log(orders)
 
 
